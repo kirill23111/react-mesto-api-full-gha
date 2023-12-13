4b9ea0b978b0a103535efeb8704e9627c516bc03
kirill23111 <radyushinkirill@yandex.ru>
2023-12-13 23:42:43 +0200

    pr15

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { celebrate, Joi } = require('celebrate');
+const cors = require('cors');
 
 const app = express();
 const { errors } = require('celebrate'); // Добавляем обработку ошибок celebrate
@@ -23,6 +24,7 @@ const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process
 mongoose.connect(MONGO_URL);
 const db = mongoose.connection;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
