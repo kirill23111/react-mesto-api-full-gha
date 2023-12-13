5d569958ac116c9fccf30f5322a22347722a1f41
kirill23111 <radyushinkirill@yandex.ru>
2023-12-13 23:02:26 +0200

    pr15

--- backend/app.js
+++ backend/app.js
@@ -53,8 +53,6 @@ app.post(
   login,
 );
 
-app.use(createProxyMiddleware({ target: 'http://api.mestoproject.nomoredomainsmonster.ru', changeOrigin: true }));
-
 db.on('error', (error) => {
   console.error('Ошибка подключения к MongoDB:', error);
 });
