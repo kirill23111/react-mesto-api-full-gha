0ddadc32f5c1f1f6b44ae3bdef646c8d1fff45a0
kirill23111 <radyushinkirill@yandex.ru>
2023-12-15 16:42:21 +0200

    ПР15

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process
 mongoose.connect(MONGO_URL);
 const db = mongoose.connection;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
