9566e2ea93e5a5859ff647070d5c1e3e413265af
kirill23111 <radyushinkirill@yandex.ru>
2023-12-16 19:20:48 +0200

    pr15

--- backend/utils/bearer.js
+++ backend/utils/bearer.js
@@ -2,7 +2,7 @@ function extractBearerToken(inputString) {
   console.log(inputString, 'inputString');
   const strArr = inputString.split(' ');
 
-  console.log(strArr, strArr);
+  console.log(strArr, 'strArr');
   if (strArr[0] === 'Bearer') {
     return strArr[1];
   }
