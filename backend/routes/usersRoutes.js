9c2c1365fb6679101a7c6227e4ba074fd7f96b06
kirill23111 <radyushinkirill@yandex.ru>
2023-12-16 20:03:17 +0200

    pr15-2

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,29 +1,3 @@
-// const express = require('express');
-// const usersController = require('../controllers/users');
-// const authMiddleware = require('../middlewares/auth');
-
-// const usersRouter = express.Router();
-
-// // Роут для получения всех пользователей
-// usersRouter.get('/users', authMiddleware, usersController.getUsers);
-
-// // Роут для получения пользователя по _id
-// usersRouter.get('/users/:userId', authMiddleware, usersController.getUserById);
-
-// // Роут для создания нового пользователя
-// // usersRouter.post('/signin', usersController.login);
-// // usersRouter.post('/signup', usersController.createUser);
-
-// // Роут для обновления профиля пользователя
-// usersRouter.patch('/users/me', authMiddleware, usersController.updateProfile);
-
-// // Роут для обновления аватара пользователя
-// usersRouter.patch('/users/me/avatar', authMiddleware, usersController.updateAvatar);
-
-// // Роут для получения информации о текущем пользователе
-// usersRouter.get('/users/me', authMiddleware, usersController.getCurrentUser);
-
-// module.exports = usersRouter;
 const express = require('express');
 const usersController = require('../controllers/users');
 const authMiddleware = require('../middlewares/auth');
