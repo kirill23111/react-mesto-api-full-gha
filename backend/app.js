bf0292aacab167788265ebe7e41123fd62de28fa
kirill23111 <radyushinkirill@yandex.ru>
2023-12-16 18:19:05 +0200

    pr15

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ mongoose.connect(MONGO_URL);
 const db = mongoose.connection;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://mestoproject.nomoredomainsmonster.ru'],
   credentials: true
 }));
 app.use(express.json());
